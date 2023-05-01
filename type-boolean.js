6806aadc2e6ab6038d46e8590ac39873c298fb72
Erasmo Mello <erasmocorreademello@gmail.com>
2023-05-01 14:39:25 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+//booleanos
+// true - verdadeiro
+//false - falso
+
+const primeiroNumber = 6;
+const segundoNumber = 6;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumber === segundoNumber);
+
+const textoPrimeiro = "Erasmo";
+const textoSegundo = "erasmo";
+
+console.log(textoPrimeiro === textoSegundo); //false
---
+++ type-string.js
@@ -0,0 +1,25 @@
+//Strings example
+
+const texto1 = "Hello, world";
+const texto2 = 'Hello, world';
+const senha = "senhaSuperSegura4321!"
+const stringDeNumeros = "7799554";
+
+const citacao = "Hi! My name is ";
+const meuNome = "Erasmo";
+
+//concatenação somente com uso do (+)
+
+console.log(citacao + meuNome);
+
+//template string or template literal
+
+
+
+
+const cidade = "erechim";
+const input = "Erechim";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); //true
